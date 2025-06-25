65299ebbb86de7301ab508b92de7120050ad5054
eccedentesiast <khanhnganle.240703@gmail.com>
2025-06-25 11:34:52 +0700

    .

--- spark_jobs/transform_data.py
+++ spark_jobs/transform_data.py
@@ -2,17 +2,19 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, to_date 
 from pyspark.sql.types import DoubleType
 from pyspark.sql.window import Window
-from pyspark.sql.functions import last
+from pyspark.sql.functions import last, lag, log,avg
 import numpy as np 
 import pandas as pd 
 import os 
 
-def transform_finance_data ():
-    input_path = 'data/raw/yahoo_finance_data.csv'
-    output_path = 'data/processed/yahoo_finance_data.csv'
-    spark = SparkSession.builder.appName("CleanDataYahooFinance").getOrCreate()
-    df = spark.read.option("header",True).option("inferSchema",True).csv(input_path)
-    #Chuyen kieu du lieu 
+def create_spark():
+    return SparkSession.builder.appName("Transform_Finance_Data").getOrCreate()
+
+def read_data(spark, path):
+    return spark.read.option("header", True).option("inferSchema", True).csv(path)
+
+def clean_finance_data (df):
+    #cast datatye 
     df = df.withColumn("Date", to_date(col("Date")))\
             .withColumn("Open", col("Open").cast(DoubleType()))\
             .withColumn("High", col("High").cast(DoubleType()))\
@@ -25,9 +27,35 @@ def transform_finance_data ():
     df = df.dropDuplicates(["Date","Ticker"])
     df = df.filter((col("Close") >=0) & (col("Volume")>=0))
     # fill missing value 
-    df.write.mode("overwrite").csv(output_path,header=True)
     print("success")
-    spark.stop()
+    return df
 
+def calculate_log_return (df):
+    window_spec = Window.partitionBy("Ticker").orderBy("Date")
+    df = df.withColumn("prev_close",lag("Close").over(window_spec))
+    df = df.withColumn("log_return",log(col("Close")/col("prev_close")))
+    return df
+
+def calculate_moving_average (df,window_size = 7):
+    window_spec = Window.partitionBy("Ticker").orderBy("Date").rowsBetween(-(window_size -1),0)
+    df = df.withColumn(f"MA_{window_size}", avg("Close").over(window_spec))
+    return df 
+
+def calculate_rate_of_change(df, n = 5):
+    window_spec = Window.partitionBy("Ticker").orderBy("Date")
+    prev_close = lag("Close",n).over(window_spec)
+    df = df.withColumn(f"roc_{n}",(((col("Close") -prev_close)/prev_close))*100)
+    return df
+
+def transform_finance_data():
+    input_path = 'data/raw/yahoo_finance_data.csv'
+    output_path = 'data/processed/yahoo_finance_data.csv'
+    spark = create_spark()
+    df = read_data(spark,input_path)
+    df = clean_finance_data(df)
+    df = calculate_rate_of_change(df,n=5)
+    df.write.mode("overwrite").csv(output_path,header=True)
+    spark.stop()
+    
 if __name__=="__main__":
     transform_finance_data()
