688d49df1b3dc823c7c853b1df37cfc5bd5e52d8
eccedentesiast <khanhnganle.240703@gmail.com>
2025-06-26 23:01:49 +0700

    .complete

--- spark_jobs/load_data.py
+++ spark_jobs/load_data.py
@@ -1,15 +1,16 @@
 from pyspark.sql import SparkSession
-import findspark
 
 def load_finance_data():
-    spark = SparkSession.builder.appName("Load_Finance_Data").getOrCreate()
-    df = spark.read.option("header",True).csv("data/processed/data/processed/yahoo_finance_data_final.csv")
-    df.write.format("jdbc").option("url","jdbc:postgresql://localhost:5432/mydatabase")\
-            .option("dbtable","mytable")\
-            .option("user","airflow")\
-            .option("password","airflow")\
-            .mode("overwrite").save()
-    spark.stop()
-
+        spark = SparkSession.builder.appName("Load_Finance_Data")\
+                    .config("spark.jars","file:///D:/finance_etl/jars/postgresql-42.2.27.jar")\
+                    .getOrCreate()
+        df = spark.read.option("header",True).csv("data/processed/yahoo_finance_data_final.csv")
+        df.write.format("jdbc").option("url","jdbc:postgresql://localhost:5432/airflow")\
+                .option("dbtable","finance")\
+                .option("user","airflow")\
+                .option("password","airflow")\
+                .option("driver", "org.postgresql.Driver")\
+                .mode("overwrite").save()
+        spark.stop()
 if __name__ == "__main__":
-    load_to_postgre()
+    load_finance_data()
